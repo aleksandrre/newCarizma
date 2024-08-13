d58ec5148147b0bad7b40f17052798fa7676ee21
aleksandrre <108459639+aleksandrre@users.noreply.github.com>
2024-08-13 23:43:50 +0400

    add addcolortoproduct route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,12 +6,15 @@ import {
   deleteProductById,
   updateProduct,
   addCategory,
+  addColorToProduct,
 } from "../controllers/adminController.js"; // Adjust the path based on your project structure
 import { authenticateToken } from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/isAdminMiddleware .js";
 
 router.post("/add-product", authenticateToken, isAdmin, addProduct);
 router.post("/add-Category", authenticateToken, isAdmin, addCategory);
+router.post("/add-color", authenticateToken, isAdmin, addColorToProduct);
+
 router.delete(
   "/delete-product/:id",
   authenticateToken,
