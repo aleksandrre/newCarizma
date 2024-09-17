622f7a2e8649eeae5e80e71af6af93c76f906421
aleksandrre <108459639+aleksandrre@users.noreply.github.com>
2024-09-18 01:44:24 +0400

    chabged

--- models/productModel.js
+++ models/productModel.js
@@ -20,7 +20,7 @@ const categorySchema = new mongoose.Schema({
   ],
 });
 
-// Defiggggne the Color schema
+// Define the Color schema
 const colorSchema = new mongoose.Schema({
   colorName: {
     type: String,
@@ -50,64 +50,49 @@ const colorSchema = new mongoose.Schema({
 });
 
 // Define the Product schema
-const productSchema = new Schema({
+const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true, // Product must have a name
   },
+  // Allow multiple categories
   categories: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category", // Reference to the Category model
+      required: true, // Each product must belong to at least one category
     },
   ],
-  LongDescription: {
+  description: {
     type: String,
-  },
-  ShortDescription: {
-    type: String,
-    required: true,
+    required: true, // Product must have a description
   },
   images: {
-    type: [String],
-    required: true,
+    type: [String], // Array of URLs or paths to product photos
+    required: true, // Product must have photos
   },
   mainPrice: {
     type: Number,
-    required: true,
-    min: 0,
+    required: true, // Main price must be provided
+    min: 0, // Main price should be non-negative
   },
   isNewProduct: {
     type: Boolean,
-    default: false,
+    default: false, // Default value is false
   },
   sale: {
     type: Number,
-    required: true,
-    min: 0,
-    max: 100,
+    required: true, // Sales percentage must be provided
+    min: 0, // Should not be negative
+    max: 100, // Should not exceed 100,
     default: 0,
   },
   isTopSale: {
     type: Boolean,
-    default: false,
+    default: false, // Default value is false
   },
-  colors: [colorSchema],
+  colors: [colorSchema], // Array of color objects
 });
-
-// Define a virtual for the newPrice with two decimal places
-productSchema.virtual("newPrice").get(function () {
-  let price = this.mainPrice;
-  if (this.sale > 0) {
-    price = this.mainPrice - (this.mainPrice * this.sale) / 100;
-  }
-  return price.toFixed(2); // Return price with two decimal places
-});
-
-// Ensure virtuals are included when converting to JSON
-productSchema.set("toJSON", { virtuals: true });
-productSchema.set("toObject", { virtuals: true });
 
 // Create the models
 export const Category = mongoose.model("Category", categorySchema);
